d79e8424d6df1730c8d26dd5ba3b893e88318243
Chaitu8553 <chaitanyayelijerla@gmail.com>
2025-02-16 18:12:08 +0530

    Update1 qwerty.py

--- qwerty.py
+++ qwerty.py
@@ -1,5 +1,5 @@
 #Inheritance pgm...
-class person:
+//class person:
     def eat(self):
         print("I can eat")
     def work(self):
@@ -13,4 +13,27 @@ class chaitu(person):
         
 male=chaitu()
 male.flirt()
-male.work()
+male.work()//
+##Program (PythonPandasEx2.py)
+#Program to demo Pandas series using Series() method
+'''
+import pandas as pd;
+x=[10,20,30,40,50]
+y=pd.Series(x)
+print(y)
+print(y[0]);
+print(y[1]);
+print(y[2]);
+print(y[3]);
+print(y[4]);
+'''
+# pandass accessing with our own labels....
+import pandas as pd;
+x=[10,20,30,40,50]
+y=pd.Series(x,index=["a","b","c","d","e"])
+print(y)
+print(y["a"]);
+print(y["b"]);
+print(y["c"]);
+print(y["d"]);
+print(y["e"]);
